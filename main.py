737a9ea355816325c68b97d898a1586edb262b2c
Shashank Nidhi <shashanknidhi617@gmail.com>
2022-12-28 01:32:10 +0530

    added the code to get transparent fg_image

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Import Statements
 import os, shutil
+import numpy as np
 from PIL import Image
 # Functions: Matting, Obj_placement, harmonization
 from matting.getMatting import Matting
@@ -22,10 +23,24 @@ def main(foreground,background):
     # Output: output_matting/fg_mask.png
     #Matting(image_path='input/foreground.png')
     Matting()
+    image = Image.open('input/foreground.png')
+    sghm = Image.open('output_matting/fg_mask.png')
+    image = np.asarray(image)
+    if len(image.shape) == 2:
+        image = image[:, :, None]
+    if image.shape[2] == 1:
+        image = np.repeat(image, 3, axis=2)
+    elif image.shape[2] == 4:
+        image = image[:, :, 0:3]
+    sghm = np.repeat(np.asarray(sghm)[:, :, None], 3, axis=2) / 255
+    foreground_sghm =  image * sghm
+    #convert array to image
+    foreground_sghm = Image.fromarray(foreground_sghm.astype('uint8'), 'RGB')
+    foreground_sghm.save('output_matting/foreground.png')
     #Object Placement
-    # Input: input/foreground.png, input/background.png, output_matting/fg_mask.png
+    # Input: output_matting/foreground.png, input/background.png, output_matting/fg_mask.png
     # Output: output_object_placement/composite_image.png output_object_placement/composite_mask.png
-    #place(foreground='input/foreground.png',background='input/background.png',fg_mask='output_matting/fg_mask.png')
+    #place(foreground='output_matting/foreground.png',background='input/background.png',fg_mask='output_matting/fg_mask.png')
     place()
     #Harmonization
     # Input: output_object_placement/composite_image.png output_object_placement/composite_mask.png
