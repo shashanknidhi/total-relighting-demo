968a6b12ae29ed9eafbdde2def3a9ccf7f718f03
Shashank Nidhi <shashanknidhi617@gmail.com>
2022-12-28 16:36:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import os, shutil
 import numpy as np
 from PIL import Image
+import requests
 # Functions: Matting, Obj_placement, harmonization
 from matting.getMatting import Matting
 from object_placement.placement import place
@@ -23,20 +24,34 @@ def main(foreground,background):
     # Output: output_matting/fg_mask.png
     #Matting(image_path='input/foreground.png')
     Matting()
-    image = Image.open('input/foreground.png')
-    sghm = Image.open('output_matting/fg_mask.png')
-    image = np.asarray(image)
-    if len(image.shape) == 2:
-        image = image[:, :, None]
-    if image.shape[2] == 1:
-        image = np.repeat(image, 3, axis=2)
-    elif image.shape[2] == 4:
-        image = image[:, :, 0:3]
-    sghm = np.repeat(np.asarray(sghm)[:, :, None], 3, axis=2) / 255
-    foreground_sghm =  image * sghm
-    #convert array to image
-    foreground_sghm = Image.fromarray(foreground_sghm.astype('uint8'), 'RGBA')
-    foreground_sghm.save('output_matting/foreground.png')
+    # image = Image.open('input/foreground.png')
+    # sghm = Image.open('output_matting/fg_mask.png')
+    # image = np.asarray(image)
+    # if len(image.shape) == 2:
+    #     image = image[:, :, None]
+    # if image.shape[2] == 1:
+    #     image = np.repeat(image, 3, axis=2)
+    # elif image.shape[2] == 4:
+    #     image = image[:, :, 0:3]
+    # sghm = np.repeat(np.asarray(sghm)[:, :, None], 3, axis=2) / 255
+    # foreground_sghm =  image * sghm
+    # #convert array to image
+    # foreground_sghm = Image.fromarray(foreground_sghm.astype('uint8'), 'RGBA')
+    # foreground_sghm.save('output_matting/foreground.png')
+    image_path = 'input/foreground.png'
+    response = requests.post(
+        'https://api.remove.bg/v1.0/removebg',
+        files={'image_file': open(image_path, 'rb')},
+        data={'size': 'auto'},
+    # add the api key here
+        headers={'X-Api-Key': 'wkBGkNWAaGfhV4N5aaY184dE'},
+    )
+    if response.status_code == requests.codes.ok:
+    # change the folder name to the folder where you want the output
+        with open(os.path.join('output_matting/foreground.png'), 'wb') as out:
+            out.write(response.content)
+    else:
+        print("Error:", response.status_code, response.text)
     #Object Placement
     # Input: output_matting/foreground.png, input/background.png, output_matting/fg_mask.png
     # Output: output_object_placement/composite_image.png output_object_placement/composite_mask.png
